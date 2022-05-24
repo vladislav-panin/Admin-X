91be3565e62a723ec10fc141101f9ca0e9897094
VladislavPanin <vladislav.o.panin@mail.ru>
2022-05-24 12:39:52 +0700

    Первый коммит. Была добавлена блок схема, а так же начальный макет проекта с mvn зависимостями

---
+++ adminx/src/main/java/ru/sibertech/adminx/AdminxApplication.java
@@ -0,0 +1,13 @@
+package ru.sibertech.adminx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminxApplication.class, args);
+	}
+
+}
---
+++ adminx/src/test/java/ru/sibertech/adminx/AdminxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sibertech.adminx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
